eaba40451b4fe72a4b5bf1fff28ec46f0606e5de
adityasundaram <don.aditya@gmail.com>
2014-11-12 17:31:43 +0530

    first commit

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.get('/', function (req, res) {
 });
 
 app.use(express.static(__dirname));
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 server.listen(2013);
 io.sockets.on('connection', function (socket){
 
